178337e4ba66339eabf6d8abab02a98c0462df1e
Teo Orthlieb <teo.orthlieb@gmail.com>
2023-04-08 17:59:25 +0200

    v1 working

--- src/fourrecar.js
+++ src/fourrecar.js
@@ -1,9 +1,11 @@
-document.body.style.border = "5px solid red";
+let newUrl = document.location.href;
+if (!document.location.href.includes('&sort=')) {
+    newUrl += '&sort=facet_price';
+}
+if (!document.location.href.includes('s?filters[Facet_vendeurs][0]')) {
+    newUrl = newUrl.replace(/s\?.*?&/, 's?filters[Facet_vendeurs][0]=Carrefour&');
+}
 
-browser.runtime.onInstalled.addListener(() => {
-    browser.contextMenus.create({
-        "id": "sampleContextMenu",
-        "title": "Sample Context Menu",
-        "contexts": ["selection"]
-    });
-});
+if (document.location.href !== newUrl) {
+    document.location = newUrl;
+}
